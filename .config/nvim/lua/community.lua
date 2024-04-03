afa0844f033f7bce544587d730e0f82a95efdb52
Thweki <thweki@foxmail.com>
2024-04-04 00:13:40 +0800

    :sparkles: Use astronvim instead of vimrc.

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,24 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  -- import/override with your plugins folder
+}
